0bb10959333511f091ed3038d63e0973e7dacfae
Ibrahimaden00 <Ibrahimaden00@gmail.com>
2021-11-10 16:17:02 +0100

    Tog bort loggen på index och privacy.cs för det behövs inte

--- Webshop/Pages/Index.cshtml.cs
+++ Webshop/Pages/Index.cshtml.cs
@@ -10,12 +10,7 @@ namespace Webshop.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public void OnGet()
         {
--- Webshop/Pages/Privacy.cshtml.cs
+++ Webshop/Pages/Privacy.cshtml.cs
@@ -10,13 +10,7 @@ namespace Webshop.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public void OnGet()
         {
         }
