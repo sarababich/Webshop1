f5cdfb2f281a9e11f444f1971499fc9852665608
sarababich <94055284+sarababich@users.noreply.github.com>
2021-11-15 20:19:23 +0100

    Add files via upload

---
+++ Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lagersaldo.Controllers
+{
+    internal class Repository
+    {
+        internal object GetAvailableStock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StockController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lagersaldo.Controllers
+{
+    public class StockController : Controller
+    {
+        private Repository _repo;
+        private object p;
+        private string productTitle;
+        private IEnumerable<object> availableProducts;
+
+        public StockController() : base()
+        {
+            _repo = new Repository();
+        }
+
+        public ActionResult Index(string productNr)
+        {
+            object availableProduct = _repo.GetAvailableStock();
+            foreach (object p in availableProducts)
+            {
+                ;
+            }
+
+            var viewModel = new ProductsInStockViewModel() { availableProducts = new List<ProductStockDBO>() };
+            {
+                if (string.IsNullOrEmpty(productNr) || productTitle == productNr)
+                {
+                }
+                else
+                {
+                    return View(viewModel);
+                }
+                viewModel.availableProducts.Add(NewMethod(p));
+                return View(viewModel);
+            }
+            return View(viewModel);
+        }
+
+        private static ProductStockDBO NewMethod(object p)
+        {
+            return new ProductStockDBO()
+            {
+                ProductTitle = "Sandal",
+                Quantity = "Quantity",
+            };
+        }
+    }
+
+    internal class ProductStockDBO
+    {
+        public object Quantity { get; set; }
+        public object ProductNr { get; set; }
+        public string ProductTitle { get; internal set; }
+    }
+
+    internal class ProductsInStockViewModel
+    {
+        public ProductsInStockViewModel()
+        {
+        }
+
+        public List<ProductStockDBO> availableProducts { get; set; }
+    }
+}
